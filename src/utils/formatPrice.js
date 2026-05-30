1632d6ce8c4291184888af63052706169a82090b
Jhnvncntx <jhnvncntx@gmail.com>
2026-05-31 05:08:04 +0800

    feat: initial React client setup with Vite, Tailwind, and auth context

---
+++ src/utils/formatPrice.js
@@ -0,0 +1,7 @@
+export const formatPrice = (price) => {
+    return new Intl.NumberFormat('en-PH', {
+      style: 'currency',
+      currency: 'PHP',
+      minimumFractionDigits: 0,
+    }).format(price);
+  };
