2bc8bb7273d7e7f15af6a622fbc3af95b8202a42
Jhnvncntx <jhnvncntx@gmail.com>
2026-05-31 15:15:21 +0800

    chore: remove debug log from axios config

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
-console.log('API baseURL:', baseURL);
-
 const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
